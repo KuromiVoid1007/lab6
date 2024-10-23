f586d45c09a4c24ddc5659846af18ef97f1d1e20
KuromiVoid1007 <156536242+KuromiVoid1007@users.noreply.github.com>
2024-10-23 22:35:23 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,15 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String[] strings = new String[5];
 
-
         System.out.println("Введите 5 строк:");
         for (int i = 0; i < 5; i++) {
             strings[i] = scanner.nextLine();
         }
 
         try {
-
             for (int i = 0; i < 5; i++) {
                 if (strings[i] == null || strings[i].isEmpty()) {
-                    System.out.println("Строка пустая");
+                    throw new NullPointerException("Строка пустая.");
                 } else {
                     System.out.println("Длина строки: " + strings[i].length());
                 }
@@ -23,29 +21,9 @@ public class Main {
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Ошибка: Обращение к несуществующему элементу массива.");
         } catch (NullPointerException e) {
-            System.out.println("Ошибка: Строка пустая.");
+            System.out.println("Ошибка: " + e.getMessage());
         } finally {
             System.out.println("Обработка завершена.");
         }
-
-        //Вариант всегда выдет ArrayIndexOutOfBoundsException
-        /* try {
-            for (int i = 0; i <= 5; i++) {
-                if (strings[i] == null ) {
-                    throw new NullPointerException();
-                }
-                if (strings[i].isEmpty()) {
-                    System.out.println("Строка пустая");
-                } else {
-                    System.out.println("Длина строки: " + strings[i].length());
-                }
-            }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Ошибка: Обращение к несуществующему элементу массива.");
-        } catch (NullPointerException e) {
-            System.out.println("Ошибка: Строка пустая.");
-        } finally {
-            System.out.println("Обработка завершена.");
-        }*/
     }
-}
+}    
